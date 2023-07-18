a85925bd50ab80dc5ee6b81d0fda173d18c182fd
jimzord12 <w.tool@yahoo.gr>
2023-07-18 18:04:13 +0300

    Updated Contract Address, due server shutDown

--- src/web3/constants/deployedContracts.js
+++ src/web3/constants/deployedContracts.js
@@ -1,6 +1,13 @@
+// export const deployedContractAddresses = {
+//   ERC20ContractAddress: "0x167b75e351e6eacAdCB515558f056C95Ec805e3B",
+//   OracleContractAddress: "0x5da6c647A79Ce540303CB6C988555F4616aB4203",
+//   ServicesContractAddress: "0x9B3fcEB7bf97fD38FA0FE71250b0cb45b0cE6942",
+//   RewardingToolAddress: "0x76be496Efe62dC94c52bE475A20159ab215deedA",
+// };
+
 export const deployedContractAddresses = {
-  ERC20ContractAddress: "0x167b75e351e6eacAdCB515558f056C95Ec805e3B",
-  OracleContractAddress: "0x5da6c647A79Ce540303CB6C988555F4616aB4203",
-  ServicesContractAddress: "0x9B3fcEB7bf97fD38FA0FE71250b0cb45b0cE6942",
-  RewardingToolAddress: "0x76be496Efe62dC94c52bE475A20159ab215deedA",
+  ERC20ContractAddress: "0x7384B00F5702Dd5d9d7cb08DC303f2534D46D3Db",
+  OracleContractAddress: "0xdF791bf449B2D5020285DC01c42410454281c2e8",
+  ServicesContractAddress: "0x5eEb3cD8B18F3BBb2F59e64b5756dE66EFa37aac",
+  RewardingToolAddress: "0x453439ad01F311097fFa96cC33C4001e6834c2c6",
 };
